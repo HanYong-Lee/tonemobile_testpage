0bdfac96d314ff772afab381314b0c48c812f092
HY_L <133779344+HanYong-Lee@users.noreply.github.com>
2026-09-16 10:25:57 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.T1_CONFIG = {
   // Google Sheets를 '웹에 게시 → CSV'로 설정한 뒤 주소를 입력하세요.
-  inventoryCsvUrl: "",
+  inventoryCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR_MPdGxEXnPAIBt7QrG33FJYWsykUPjWTRUivNJ0_J3AGU3ul0l2RLG5lU2pJ_JepFUnCqplFwNQ0X/pub?gid=0&single=true&output=csv",
   storesCsvUrl: "",
   // CTA 기록용 Google Apps Script 웹 앱 주소를 입력하세요.
   analyticsEndpoint: "",
